5e4230bc310c24fb840e9a5bc6e6064d2c044869
Luna Mendes <lkmnds@gmail.com>
2018-07-21 17:08:41 -0300

    Revert "use ws settings for gateway endpoint"
    This reverts commit cb708422809fd69885ee26da917315f75952fae7.
    
    WEBSERVER_URL was made for the case where litecord is behind
    a reverse proxy (nginx / cloudflare, etc). If the instance
    isn't behind a reverse proxy, you can set WEBSERVER_URL to your
    own local address.

--- litecord/blueprints/gateway.py
+++ litecord/blueprints/gateway.py
@@ -7,9 +7,7 @@ bp = Blueprint('gateway', __name__)
 
 def get_gw():
     proto = 'wss://' if app.config['IS_SSL'] else 'ws://'
-    ws_host = app.config['WS_HOST']
-    ws_port = app.config['WS_PORT']
-    return f'{proto}{ws_host}:{ws_port}/ws'
+    return f'{proto}{app.config["WEBSERVER_URL"]}/ws'
 
 
 @bp.route('/gateway')
