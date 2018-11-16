800da730df2045e1c0a213bd9f6f352ad2352091
Luna Mendes <luna@elixi.re>
2018-11-16 16:54:20 -0300

    litecord.jobs: add wrapper for logging errors

--- litecord/jobs.py
+++ litecord/jobs.py
@@ -1,5 +1,8 @@
 import asyncio
 
+from logbook import Logger
+log = Logger(__name__)
+
 
 class JobManager:
     """Manage background jobs"""
@@ -7,8 +10,16 @@ class JobManager:
         self.loop = loop or asyncio.get_event_loop()
         self.jobs = []
 
+    async def _wrapper(self, coro):
+        try:
+            await coro
+        except Exception:
+            log.exception('Error while running job')
+
     def spawn(self, coro):
-        task = self.loop.create_task(coro)
+        task = self.loop.create_task(
+            self._wrapper(coro)
+        )
         self.jobs.append(task)
 
     def close(self):
