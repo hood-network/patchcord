54fc80ea4ef6ed9c69f4025d6e5e3c36cf5bf39a
Luna Mendes <git@l4.pm>
2018-11-21 22:06:10 -0300

    tests.test_main: remove 'hewwo' content check

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,3 @@ async def test_index(test_cli):
     """Test if the main index page works."""
     resp = await test_cli.get('/')
     assert resp.status_code == 200
-    assert (await resp.get_data()).decode() == 'hewwo'
