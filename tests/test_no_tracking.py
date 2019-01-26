53d7515f8c953fd36f8d8268675b9e1f8e3cf4a0
Luna <git@l4.pm>
2019-01-26 19:25:46 -0300

    all: update copyright year

--- tests/test_no_tracking.py
+++ tests/test_no_tracking.py
@@ -1,7 +1,7 @@
 """
 
 Litecord
-Copyright (C) 2018  Luna Mendes
+Copyright (C) 2018-2019  Luna Mendes
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
