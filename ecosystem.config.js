6827259d307be9095959ac3c41942852b07df3ba
Luna <git@l4.pm>
2019-02-26 23:23:06 -0300

    ecosystem.config.js: add bind

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "litecord",
-        script: "pipenv run hypercorn run:app",
+        script: "pipenv run hypercorn run:app --bind 0.0.0.0:5000",
         watch: true,
       }
   ]
