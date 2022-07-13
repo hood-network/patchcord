bc1d11aec3748a537a8343aadc1707893fe18b2c
dolfies <jeyalfie47@gmail.com>
2022-07-13 12:49:03 -0400

    Revert "Attempt to speed up assets by redirecting"
    This reverts commit 5c5f5882d26feae9c96b8ef3150a4239ccac9e2a.

--- litecord/blueprints/static.py
+++ litecord/blueprints/static.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
-from quart import Blueprint, current_app as app, render_template, make_response, redirect
+from quart import Blueprint, current_app as app, render_template, make_response
 from pathlib import Path
 import aiohttp
 import time
@@ -38,12 +38,11 @@ async def static_pages(path):
 @bp.route("/assets/<asset>", methods=["GET"])
 async def proxy_asset(asset):
     """Proxy asset requests to Discord."""
-    return redirect(f"https://discord.com/assets/{asset}", code=301)
-    # async with aiohttp.request("GET", f"https://canary.discord.com/assets/{asset}") as resp:
-    #     response = await make_response(await resp.read())
-    #     response.status = resp.status
-    #     response.headers['content-type'] = resp.headers['content-type']
-    #     return response
+    async with aiohttp.request("GET", f"https://canary.discord.com/assets/{asset}") as resp:
+        response = await make_response(await resp.read())
+        response.status = resp.status
+        response.headers['content-type'] = resp.headers['content-type']
+        return response
 
 
 def _get_environment(app):
