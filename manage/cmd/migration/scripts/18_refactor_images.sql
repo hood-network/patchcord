177d7cc7e69dd83674c9955c199626f756cb9e8c
dolfies <jeyalfie47@gmail.com>
2022-07-15 12:51:37 -0400

    I hate sql

--- manage/cmd/migration/scripts/18_refactor_images.sql
+++ manage/cmd/migration/scripts/18_refactor_images.sql
@@ -1,15 +1,27 @@
 UPDATE icons
     SET scope = 'user_avatar'
     WHERE scope = 'avatar';
+
+UPDATE icons
     SET scope = 'user_banner'
     WHERE scope = 'banner';
+
+UPDATE icons
     SET scope = 'guild_icon'
     WHERE scope = 'guild';
+
+UPDATE icons
     SET scope = 'guild_splash'
     WHERE scope = 'splash';
+
+UPDATE icons
     SET scope = 'guild_discovery_splash'
     WHERE scope = 'discovery_splash';
+
+UPDATE icons
     SET scope = 'guild_banner'
     WHERE scope = 'banner';
+
+UPDATE icons
     SET scope = 'channel_icon'
     WHERE scope = 'channel-icons';
