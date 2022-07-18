fd6d45171dd227010dc32f23c9393774e19c315d
dolfies <jeyalfie47@gmail.com>
2022-07-17 20:53:18 -0400

    i fucked up

--- manage/cmd/migration/scripts/21_fuck_icons_2.sql
+++ manage/cmd/migration/scripts/21_fuck_icons_2.sql
@@ -1,7 +1,7 @@
 UPDATE icons 
     SET hash = 'a_78f36f55ba85d65b.' || REPLACE (hash, '#a_', '')
-    WHERE hash = hash;
+    WHERE hash LIKE '#a_%';
 
 UPDATE icons 
     SET hash = '78f36f55ba85d65b.' || REPLACE (hash, '#', '')
-    WHERE hash = hash;
+    WHERE hash NOT LIKE '%a_%';
